74ab9dd7aa26b4e9a0bddb03abaea0d5a301f78b
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-18 12:35:15 +0100

    Typography

--- chap14/ex1.rb
+++ chap14/ex1.rb
@@ -12,11 +12,11 @@ puts prompt
 lives = $stdin.gets.chomp
 
 # a comma for puts is like using it twice
-puts "What kind of computer do you have? " prompt
+puts "What kind of computer do you have? ", prompt
 computer = $stdin.gets.chomp
 
 puts """
-Alright, so you said you #{likes} about liking me.
-You live in #{lives}. Not sure where that is.
-And you have a #{computer} computer. Nice.
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
 """
