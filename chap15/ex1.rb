d6be4b4bd758c1eddd6643df477e0ed130cce247
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-18 13:05:38 +0100

    ex 15 with annotations

--- chap15/ex1.rb
+++ chap15/ex1.rb
@@ -1,13 +1,21 @@
+# store the first argument from the command line in 'filename' variable
 filename = ARGV.first
 
+# store the contents of filename in 'txt' variable
 txt = open(filename)
 
+# hint to the user about what's going on
 puts "Here's your file #{filename}:"
+# apply the 'read' method, which is a File method, to txt
 print txt.read
 
+# user prompt
 puts "Type the filename again: "
+# get a new file name from user input
 file_again = $stdin.gets.chomp
 
+# as above but with user submitted file
 txt_again = open(file_again)
 
+# ... and again
 print txt_again.read
