eb5935a318f2012e6a290c8974f112429e6e2698
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-18 13:44:30 +0100

    Refactor ex 16 to use fewer lines to take input and save to test.txt

--- chap16/ex1.rb
+++ chap16/ex1.rb
@@ -21,21 +21,15 @@ target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
-print "line 1: "
-line1 = $stdin.gets.chomp
-print "line 2: "
-line2 = $stdin.gets.chomp
-print "line 3: "
-line3 = $stdin.gets.chomp
+lines = ''
+
+3.times do
+  print "line: "
+  lines += $stdin.gets.chomp + "\n"
+end
 
 puts "I'm going to write these to the file."
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(lines)
 
 puts "And finally, we close it."
 target.close
