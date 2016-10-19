1a20a7765bcc29dd7484ea8ed7cb32afc4353b44
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-19 15:40:42 +0100

    ex33 refactored as method

---
+++ chap33/ex1.rb
@@ -0,0 +1,19 @@
+def gt_check(n)
+  i = 0
+  numbers = []
+
+  while i < n
+    puts "At the top i is #{i}"
+    numbers.push(i)
+
+    i += 1
+    puts "Numbers now: ", numbers
+    puts "At the bottom i is #{i}"
+  end
+
+  puts "The numbers: "
+
+  # remember you can write this 2 other ways?
+  numbers.each {|num| puts num }
+
+end
