aa8e108727541736bac1b971c8038df6bf4a485f
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-19 11:23:04 +0100

    work out study drill 2

--- chap21/ex1.rb
+++ chap21/ex1.rb
@@ -32,6 +32,8 @@ puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
 # A puzzle for the extra credit, type it in anyway.
 puts "Here is a puzzle."
 
+
+# 35 + (74 - (180 * (50/2))))
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 puts "That becomes: #{what}. Can you do it by hand?"
